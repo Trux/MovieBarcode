d14c21fe178cec9cbfd095da9185183b34b4209e
Truxs <clyraud@yahoo.fr>
2013-02-02 16:05:21 +0100

    first commit for testing purpose

---
+++ #!usr:bin:python.py
@@ -0,0 +1,11 @@
+#!usr/bin/python
+#-*-coding:utf-8-*-
+
+import sys,os,res
+
+moviePath = /Users/christophe/Movies/201211_musk.mp4 
+savePath = /Users/christophe/Movies/musk.jpg 
+movieResolution =
+framesNumber =
+tempFolder = /Users/christophe/Movies/tmp/
+
