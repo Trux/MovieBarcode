fbc9b56c688eaebaadfbc688669f6491bdda4b26
Truxs <clyraud@yahoo.fr>
2013-02-02 17:22:31 +0100

    finalized screenshot function

--- movieBarcode.py
+++ movieBarcode.py
@@ -1,11 +1,11 @@
 #!usr/bin/python
 #-*-coding:utf-8-*-
 
-import sys,os
+import sys,os,time
 from converter import Converter
-
+tps1 = time.clock()
 moviePath = '/Users/christophe/Movies/201211_musk.mp4'
-savePath = '/Users/christophe/Movies/musk.jpg '
+savePath = '/Users/christophe/Movies/musk.jpg'
 framesNumber = 1000
 tempFolder = '/Users/christophe/Movies/tmp/'
 
@@ -21,7 +21,16 @@ movieHeight = info.video.video_height
 
 
 print 'durée: %i secondes' % movieDuration
-print 'résolution: %ix%i' % (movieHeight,movieWidth)
-movieResolution = 'résolution: %ix%i' % (movieWidth,movieHeight)
+print 'résolution: %ix%i' % (movieWidth,movieHeight)
+movieResolution = '%ix%i' % (movieWidth,movieHeight)
+frameScale = round(movieDuration/framesNumber,2)
+print frameScale
+frameTime = 1
+#get asked number of movie frames
+for i in range (0,1000):
+	movie.thumbnail(moviePath, frameTime, '/Users/christophe/Movies/tmp/%i.jpeg' %i,movieResolution)
+	frameTime=frameTime+frameScale
+	#print 'frame %i at %f seconds' %(i,round(frameTime,2))
 
-movie.thumbnail('/Users/christophe/Movies/201211_musk.mp4', 10, '/Users/christophe/Movies/musk.jpg', '640x360')
+tps2 = time.clock()
+print(tps2 - tps1)
