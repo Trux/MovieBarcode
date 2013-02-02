7d12d64e84c91f5558b7693984f1069a4822d84e
Truxs <clyraud@yahoo.fr>
2013-02-02 20:51:41 +0100

    added crop function

--- movieBarcode.py
+++ movieBarcode.py
@@ -3,13 +3,15 @@
 
 import sys,os,time
 from converter import Converter
+from PIL import Image
 tps1 = time.clock()
-moviePath = '/Users/christophe/Movies/201211_musk.mp4'
+moviePath = '/Users/christophe/Movies/The Social Network (2010) [1080]/The Social Network (2010) 1080p BrRip x264 - 1.2GB - YIFY.mp4'
 savePath = '/Users/christophe/Movies/musk.jpg'
 framesNumber = 1000
-frameTime = 1
-tempFolder = '/Users/christophe/Movies/tmp/'
-
+frameTime = 0
+tempFolder = '/Users/christophe/Movies/temporary/'
+if not os.path.isdir(tempFolder):
+   os.makedirs(tempFolder)
 
 movie = Converter()
 
@@ -29,10 +31,18 @@ print frameScale
 
 
 #get asked number of movie frames
-for i in range (0,framesNumber):
-	movie.thumbnail(moviePath, frameTime, '/Users/christophe/Movies/tmp/%i.jpeg' %i,movieResolution)
+for i in range (0,movieWidth-1):
+	movie.thumbnail(moviePath, frameTime, tempFolder+'%i.jpeg' %i,movieResolution)
 	frameTime=frameTime+frameScale
-	#print 'frame %i at %f seconds' %(i,round(frameTime,2))
+	print 'frame %i at %f seconds' %(i,round(frameTime,2))
+	#print 'width %i'%w
+	#print 'height %i'%h
+	# left, upper, right, and lower 
+	image = Image.open("/Users/christophe/Movies/temporary/%i.jpeg"%i)
+	w,h = image.size
+	image.crop((0,h-h,1,h)).save("/Users/christophe/Movies/temporary/A%i.jpeg"%i)
+	print 'frame %i cropped' %(i)
+
 
 tps2 = time.clock()
 print(tps2 - tps1)
